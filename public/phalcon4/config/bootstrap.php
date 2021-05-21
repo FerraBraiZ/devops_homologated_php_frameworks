236f1365793480540e4127b9930c336eb9b8341d
Angelo Weiss <angelo.weiss@madeiramadeira.com.br>
2021-05-21 20:09:25 -0300

    phalcon4 adjust bootstrap

--- public/phalcon4/config/bootstrap.php
+++ public/phalcon4/config/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-#declare(strict_types=1);
+declare(strict_types=1);
 ini_set('ignore_repeated_errors', 'On');
 ini_set('html_errors', 'On');
 ini_set('display_errors', 'On');
@@ -11,70 +11,69 @@ defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
 
 use Phalcon\Di\FactoryDefault as container;
 use \Phalcon\Mvc\Application;
-use Dotenv\Dotenv;
 
+if (!file_exists('../vendor/autoload.php')) {
+    throw new Error("Failed to load composer's vendor autoload");
+}
+require_once '../vendor/autoload.php';
 
+/**
+ * Include Environment
+ */
 
-    if (!file_exists('../vendor/autoload.php')) {
-        throw new Error("Failed to load composer's vendor autoload");
-    }
-    require_once '../vendor/autoload.php';
+if ( !file_exists(BASE_PATH.'/config/environment.php') ) {
+    throw new Error("Failed to include environment.php");
+}
+require_once BASE_PATH.'/config/environment.php';
 
+/**
+ * The FactoryDefault Dependency Injector automatically registers
+ * the services that provide a full stack framework.
+ */
+$dic = new container();
 
-    if ( empty( $env=(new Dotenv('../environment/', 'development.env'))->load()) ){
-        throw new Error("Failed to load environment file:development.env'");
-    }
+/**
+ * Handle routes
+ */
+if ( !file_exists(BASE_PATH.'/config/router.php') ){
+    throw new Error("Failed to include router.php");
+}
+require_once BASE_PATH.'/config/router.php';
+
+/**
+ * Read services
+ */
+if ( !file_exists(BASE_PATH.'/config/services.php') ) {
+    throw new Error("Failed to include services.php");
+}
+require_once BASE_PATH.'/config/services.php';
+
+/**
+ * Get config service for use in inline setup below
+ */
+$config = $dic->getConfig();
+
+/**
+ * Include Autoloader
+ */
+if ( !file_exists(BASE_PATH.'/config/loader.php') ) {
+    throw new Error("Failed to include loader.php");
+}
+require_once BASE_PATH.'/config/loader.php';
+
+try {
 
     /**
-     * The FactoryDefault Dependency Injector automatically registers
-     * the services that provide a full stack framework.
+     * Handle the request
      */
-    $dic = new container();
+    $application = new Application($dic);
 
-//    echo "<pre>";
-//    print_r( dirname(__DIR__) );
-//    print_r($dic);
-//    echo "</pre>";
+    echo $application->handle()->getContent();
 
-    /**
-     * Handle routes
-     */
-    if ( !file_exists(BASE_PATH.'/config/router.php') ){
-        throw new Error("Failed to include router.php");
-    }
-    require_once BASE_PATH.'/config/router.php';
-//
-//    /**
-//     * Read services
-//     */
-//    if (!file_exists('services.php')) {
-//        throw new Error("Failed to include services.php");
-//    }
-//    require_once 'services.php';
-//
-//    /**
-//     * Get config service for use in inline setup below
-//     */
-//    $config = $dic->getConfig();
-//
-//    /**
-//     * Include Autoloader
-//     */
-//    if (!file_exists('loader.php')) {
-//        throw new Error("Failed to include loader.php");
-//    }
-//    require_once 'loader.php';
-//
-//try {
-//    /**
-//     * Handle the request
-//     */
-//    $application = new Application($dic);
-//
-//    echo $application->handle()->getContent();
-//
-//} catch (\Throwable $t) {
-//    echo $t->getMessage() . PHP_EOL;
-//    echo '<pre>' . $t->getTraceAsString() . '</pre>';
-//}
+} catch (\Throwable $t) {
+
+    echo $t->getMessage().PHP_EOL;
+    echo '<pre>' . $t->getTraceAsString() . '</pre>';
+
+}
 

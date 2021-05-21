2e0c1da07f65e152f25261c7b44e4271a566ff89
Angelo Weiss <angelo.weiss@madeiramadeira.com.br>
2021-05-21 19:17:05 -0300

    phalcon4 fix

--- public/phalcon4/config/bootstrap.php
+++ public/phalcon4/config/bootstrap.php
@@ -14,7 +14,6 @@ defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
 use Phalcon\Di\FactoryDefault as container;
 use \Phalcon\Mvc\Application;
 
-try {
 
     if (!file_exists('../vendor/autoload.php')) {
         throw new Error("Failed to load composer's vendor autoload");
@@ -33,51 +32,52 @@ try {
 
     require_once 'environment.php';
 
-
-    /**
-     * The FactoryDefault Dependency Injector automatically registers
-     * the services that provide a full stack framework.
-     */
-    $dic = new container();
-
-    /**
-     * Handle routes
-     */
-    if (!file_exists('router.php')) {
-        throw new Error("Failed to include router.php");
-    }
-    require_once 'router.php';
-
-    /**
-     * Read services
-     */
-    if (!file_exists('services.php')) {
-        throw new Error("Failed to include services.php");
-    }
-    require_once 'services.php';
-
-    /**
-     * Get config service for use in inline setup below
-     */
-    $config = $dic->getConfig();
-
-    /**
-     * Include Autoloader
-     */
-    if (!file_exists('loader.php')) {
-        throw new Error("Failed to include loader.php");
-    }
-    require_once 'loader.php';
-
-    /**
-     * Handle the request
-     */
-    $application = new Application($dic);
-
-    echo $application->handle()->getContent();
-
-} catch (\Throwable $t) {
-    echo $t->getMessage() . PHP_EOL;
-    echo '<pre>' . $t->getTraceAsString() . '</pre>';
-}
+//
+//    /**
+//     * The FactoryDefault Dependency Injector automatically registers
+//     * the services that provide a full stack framework.
+//     */
+//    $dic = new container();
+//
+//    /**
+//     * Handle routes
+//     */
+//    if (!file_exists('router.php')) {
+//        throw new Error("Failed to include router.php");
+//    }
+//    require_once 'router.php';
+//
+//    /**
+//     * Read services
+//     */
+//    if (!file_exists('services.php')) {
+//        throw new Error("Failed to include services.php");
+//    }
+//    require_once 'services.php';
+//
+//    /**
+//     * Get config service for use in inline setup below
+//     */
+//    $config = $dic->getConfig();
+//
+//    /**
+//     * Include Autoloader
+//     */
+//    if (!file_exists('loader.php')) {
+//        throw new Error("Failed to include loader.php");
+//    }
+//    require_once 'loader.php';
+//
+//try {
+//    /**
+//     * Handle the request
+//     */
+//    $application = new Application($dic);
+//
+//    echo $application->handle()->getContent();
+//
+//} catch (\Throwable $t) {
+//    echo $t->getMessage() . PHP_EOL;
+//    echo '<pre>' . $t->getTraceAsString() . '</pre>';
+//}
 

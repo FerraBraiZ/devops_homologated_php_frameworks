540f298127714b42ba9b5e104ba88c62fcd573de
Angelo Weiss <angelo.weiss@madeiramadeira.com.br>
2021-05-21 20:25:34 -0300

    phalcon4 adjust bootstrap

--- public/phalcon4/config/bootstrap.php
+++ public/phalcon4/config/bootstrap.php
@@ -11,7 +11,6 @@ setlocale(LC_ALL, 'ptb', 'portuguese-brazil', 'pt-br', 'bra', 'brazil');
 defined('BASE_PATH') || define('BASE_PATH', dirname(__DIR__));
 defined('APP_PATH') || define('APP_PATH', BASE_PATH . '/app');
 
-
 use \Phalcon\Mvc\Application;
 
 if (!file_exists('../vendor/autoload.php')) {
@@ -27,8 +26,6 @@ if ( !file_exists(BASE_PATH.'/config/environment.php') ) {
     throw new Error("Failed to include environment.php");
 }
 require_once BASE_PATH.'/config/environment.php';
-
-
 
 /**
  * Handle routes
@@ -55,6 +52,10 @@ if(!isset($container)){
  * Get config service for use in inline setup below
  */
 $config = $container->getConfig();
+
+echo '<pre>';
+print_r( $config );
+die;
 
 /**
  * Include Autoloader
